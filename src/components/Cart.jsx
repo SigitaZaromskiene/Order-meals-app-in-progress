0fd8d01eafad2033de18c543b7a5f54a00c6062a
SigitaZaromskiene <sigitazaromskiene@yahoo.com>
2023-06-29 17:39:49 +0300

    img

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,13 @@
 import styles from "../css/Cart.module.css";
+import ReactDOM from "react-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 function Cart() {
   return (
     <>
       <div className={styles.cart}>
+        <FontAwesomeIcon icon={faCartShopping} />
         <p>Your Cart</p>
         <p className={styles.number}>0</p>
       </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,16 @@
 import Cart from "./Cart";
 import styles from "../css/Header.module.css";
+import { food } from "../img/food.jpg";
 
 function Header() {
   return (
-    <header className={styles["header-content"]}>
-      <h2>ReactMeals</h2>
-      <Cart></Cart>
-    </header>
+    <div>
+      <header className={styles["header-content"]}>
+        <h2>ReactMeals</h2>
+        <Cart></Cart>
+      </header>
+      <img src={food} alt="Food" />
+    </div>
   );
 }
 
